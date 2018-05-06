f0bc2649222a36ca5b05285dfd2bb083774ff0fb
root <info@cacophony.org.nz>
2018-05-07 10:49:12 +1200

    New cacophony.pkg_installed_from_github custom state
    This ensures a specific version of a Cacophony Project package is installed. It
    will asked systemd to reload its configuration if a new version is installed.
    
    This state works around the issue of pkg.installed always download packages
    even if they're already installed.

---
+++ salt/_states/cacophony.py
@@ -0,0 +1,36 @@
+
+def pkg_installed_from_github(name, version, systemd_reload=True):
+    """Install a deb pacakge from a Cacophony Project Github release if it
+    isn't installed on the system already. Currently only ARM packages are
+    installed.
+
+    If a new version is installed, systemd will be asked to reload it's
+    configuration so that any new service files in the package are known to
+    systemd.
+    """
+
+    installed_version = __salt__['pkg.version'](name)
+    if installed_version == version:
+        return {
+            'name': name,
+            'result': True,
+            'comment': 'Version %s already installed.' % version,
+            'changes': {}
+        }
+
+    source_url = 'https://github.com/TheCacophonyProject/{name}/releases/download/v{version}/{name}_{version}_arm.deb'.format(
+        name=name,
+        version=version,
+    )   
+    ret = __states__['pkg.installed'](
+        name=name, 
+        version=version, 
+        sources=[{name: source_url}],
+    )
+
+    if systemd_reload and ret['result'] and ret['changes'] and not __opts__['test']:
+        __salt__['cmd.run']('systemctl daemon-reload')
+        ret['comment'] += ' (systemd reloaded)'
+
+    return ret
+
