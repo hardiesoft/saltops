56a12add53373a661c81bb9e937baf30b12ec8a9
Jon Hardie <jon@hardiesoft.com>
2020-12-03 11:41:19 +1300

    Exec from node

--- .github/workflows/run.js
+++ .github/workflows/run.js
@@ -1,7 +1,6 @@
 import yaml from "yaml";
 import fs from "fs";
-
-process.chdir("../../");
+import {exec} from "child_process";
 
 const getFilesWithExtension = (path, ext) => {
     const items = fs.readdirSync(path)
@@ -38,45 +37,67 @@ const findKeyInObject = (obj, key) => {
 }
 
 const versionData = {};
-for (const path of slsFiles) {
-    const data = fs.readFileSync(path, "utf8");
-    try {
-        const yamlData = yaml.parse(data);
-        const versionInfo = findKeyInObject(yamlData, "cacophony.pkg_installed_from_github");
-        const name = versionInfo.find(item => item.hasOwnProperty("name")).name;
-        const version = versionInfo.find(item => item.hasOwnProperty("version")).version;
-        versionData[name] = version;
-    } catch (e) {}
-}
-
-// Output the text to the README.md file, if the version info has changed since last time.
 const now = new Date();
 const separator = "____\n";
 let versionOutput = "";
-let prevVersionOutput = ""
-for (const [key, val] of Object.entries(versionData)) {
-    versionOutput += ` * ${key}: ${val}\n`;
-}
-const readme = fs.readFileSync("README.md", "utf8");
-const versionInfoStart = readme.indexOf("\n\n#### Version information");
+let prevVersionOutput = "";
 
-let output;
-if (versionInfoStart !== -1) {
-    output = readme.substring(0, versionInfoStart);
-    prevVersionOutput = readme.substring(readme.indexOf(separator) + separator.length);
-} else {
-    output = readme;
+process.chdir("../../../");
+// For each branch:
+const branches = ["dev", "test", "prod"];
+for (const branch of branches) {
+    process.chdir(`./${branch}`);
+    const slsFiles = getFilesWithExtension(".", "sls");
+    versionData[branch] = {};
+    for (const path of slsFiles) {
+        const data = fs.readFileSync(path, "utf8");
+        try {
+            const yamlData = yaml.parse(data);
+            const versionInfo = findKeyInObject(yamlData, "cacophony.pkg_installed_from_github");
+            const name = versionInfo.find(item => item.hasOwnProperty("name")).name;
+            const version = versionInfo.find(item => item.hasOwnProperty("version")).version;
+            versionData[branch][name] = version;
+        } catch (e) {}
+    }
+    process.chdir("../");
 }
-output += "\n\n#### Version information ";
-output += `(_Updated ${now.toLocaleDateString("en-NZ", { timeZone: "Pacific/Auckland" })}, ${now.toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" })}_):\n`;
-output += separator;
-output += versionOutput;
-if (versionOutput !== prevVersionOutput) {
-    fs.writeFileSync("README.md", output);
-} else {
-    // Version info is unchanged.
-    console.log("unchanged");
+
+// Output the text to the README.md file, if the version info has changed since last time.
+for (const branch of branches) {
+    versionOutput += `#### Branch \`${branch}\`\n`;
+    for (const [key, val] of Object.entries(versionData[branch])) {
+        versionOutput += ` * ${key}: ${val}\n`;
+    }
 }
-process.exit(0);
+
+for (const branch of branches) {
+    process.chdir(`./${branch}`);
+    const readme = fs.readFileSync("README.md", "utf8");
+    const versionInfoStart = readme.indexOf("\n\n#### Version information");
+    let output;
+    if (versionInfoStart !== -1) {
+        output = readme.substring(0, versionInfoStart);
+        prevVersionOutput = readme.substring(readme.indexOf(separator) + separator.length);
+    } else {
+        output = readme;
+    }
+    output += "\n\n#### Version information ";
+    output += `(_Updated ${now.toLocaleString("en-NZ", { timeZone: "Pacific/Auckland" })}_):\n`;
+    output += separator;
+    output += versionOutput;
+    if (versionOutput !== prevVersionOutput) {
+        fs.writeFileSync("README.md", output);
+        exec("git config user.name github-actions");
+        exec("git config user.email github-actions@github.com");
+        exec("git add .");
+        exec("git commit -m \"updated version information\"");
+        exec("git push");
+
+    } else {
+        // Version info is unchanged.
+        console.log("unchanged");
+    }
+    process.chdir("../");
+}
 
 
